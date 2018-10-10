ef9332754161ac695ca67e0ac949ad6d9fcddeef
Jasper Bernales <bernalesjasper@gmail.com>
2018-10-10 23:55:53 +0800

    tests

---
+++ test/test.js
@@ -0,0 +1,34 @@
+const chai = require('chai')
+const {
+  multiplyArray,
+  filterOdd,
+  flatArray,
+  sumArray
+}  = require('../main')
+
+describe('Array Functions', () => {
+  it('should flat array', () => {
+
+  })
+  it('should multiply array by 2', () => {
+
+  })
+  it('should filter array by odd numbers', () => {
+
+  })
+  it('should sum array', () => {
+
+  })
+})
+
+describe('Context Functions', () => {
+  it('should print info', () => {
+
+  })
+})
+
+describe('Async Functions', () => {
+  it('should fetch user\'s posts', async() => {
+
+  })
+})
